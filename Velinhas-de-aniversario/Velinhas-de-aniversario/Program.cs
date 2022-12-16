bdf8011761d5dad369d77fdeb281c5565f71a6d8
bia miranda <112702152+bia-miranda@users.noreply.github.com>
2022-12-16 13:37:30 -0300

    Add files via upload

---
+++ Velinhas-de-aniversario/Velinhas-de-aniversario/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+double anonasc = 0;
+double anoatual = 0;
+double idade = 0;
+
+Console.WriteLine("----QUANTAS VELINHAS POR NO BOLO?----");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("Em que ano você nasceu?");
+anonasc = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ótimo! Agora diga em que ano estamos.");
+anoatual = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------");
+idade = anoatual - anonasc;
+Console.WriteLine("Parabéns!");
+Console.WriteLine($"Seu bolo deve ter " + idade + " velinhas");
+
+Console.ReadKey();
